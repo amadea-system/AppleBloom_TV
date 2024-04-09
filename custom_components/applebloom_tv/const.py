1b579a512d43bac29207d351f3065cdf2c83518f
Amadea <53259552+amadea-system@users.noreply.github.com>
2024-04-09 13:08:11 +0000

    Add in files from the Core AppleTV Integration and remove the template component files

--- custom_components/applebloom_tv/const.py
+++ custom_components/applebloom_tv/const.py
@@ -7,3 +7,9 @@ NAME = "AppleBloom TV"
 DOMAIN = "applebloom_tv"
 VERSION = "0.0.0"
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
+
+## -- From Apple TV Integration Code -- ##
+CONF_CREDENTIALS = "credentials"
+CONF_IDENTIFIERS = "identifiers"
+
+CONF_START_OFF = "start_off"
